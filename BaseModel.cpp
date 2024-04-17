c30b6eafdabbb7586975c0790bb43595498d940e
Jc 付 <144083140+fjc2005@users.noreply.github.com>
2024-04-17 23:09:40 +0800

    A head program of 'BaseModel' class

---
+++ BaseModel.cpp
@@ -0,0 +1,19 @@
+#include "BaseModel.h"
+
+int main() {
+	const std::string MODEL_DIR = "E:\\Jupyter\\RCAN\\RCAN.pt";
+	BaseModel model(MODEL_DIR);
+
+	at::Tensor input = torch::ones({ 1, 3, 256, 256 });
+	at::Tensor output = model.forward(input);
+	std::cout << output.sizes();
+	/*torch::jit::script::Module model;
+	model = torch::jit::load(MODEL_DIR);
+
+	std::vector<torch::jit::IValue> inputs;
+	inputs.push_back(torch::ones({ 1, 3, 128, 256 }));
+
+	at::Tensor output = model.forward(inputs).toTensor();*/
+
+	return 0;
+}
---
+++ BaseModel.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <torch/script.h>
+
+class BaseModel {
+private:
+	torch::jit::script::Module body;
+	std::string MODEL_DIR;
+
+public:
+	BaseModel(std::string);
+	std::string _MODEL_DIR();
+	at::Tensor forward(at::Tensor);
+	at::Tensor forward(std::vector<torch::jit::IValue>);
+	void load();
+};
+
+BaseModel::BaseModel(std::string dir) {
+	this->MODEL_DIR = dir;
+	this->load();
+}
+
+std::string BaseModel::_MODEL_DIR() {
+	return this->MODEL_DIR;
+}
+
+void BaseModel::load() {
+	this->body = torch::jit::load(this->MODEL_DIR);
+}
+
+at::Tensor BaseModel::forward(at::Tensor input) {
+	std::vector<torch::jit::IValue> inputs;
+	inputs.push_back(input);
+	at::Tensor output = this->body.forward(inputs).toTensor();
+
+	return output;
+}
+
+at::Tensor BaseModel::forward(std::vector<torch::jit::IValue> inputs) {
+	at::Tensor output = this->body.forward(inputs).toTensor();
+	return output;
+}
